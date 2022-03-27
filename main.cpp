f1defc47fefe51f27b4f575096612830ff8beba2
PaPr <36095960+PaPaPR@users.noreply.github.com>
2022-03-27 21:58:38 +0800

    style: motify code indentation and remove waitkey

--- main.cpp
+++ main.cpp
@@ -86,136 +86,134 @@ void topCameraThread(RoboInf &robo_inf,
 
     //选择检测方框在视野中最下位置的块
     if (rectFilter(res, src_img, object_2d_rect, yolo_res_selected_id)) {
-      switch (robo_inf.catch_cube_mode_status.load())
-      {
-      case CatchMode::spin: {
-        cv::putText(src_img,
-                    "spin mode",
-                    cv::Point(50, 50),
-                    cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 150, 255), 1);
-        pnp->solvePnP(object_3d_rect, object_2d_rect, pnp_angle,
-                      pnp_coordinate_mm, pnp_depth);
-        pnp_angle.y += cube_target_yaw_angle_offset;
+      switch (robo_inf.catch_cube_mode_status.load()) {
+        case CatchMode::spin: {
+          cv::putText(src_img,
+                      "spin mode",
+                      cv::Point(50, 50),
+                      cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 150, 255), 1);
+          pnp->solvePnP(object_3d_rect, object_2d_rect, pnp_angle,
+                        pnp_coordinate_mm, pnp_depth);
+          pnp_angle.y += cube_target_yaw_angle_offset;
 
-        if (cube_middle_detect_times < cube_targeted_detect_flag_times) {
-          if (pnp_angle.y < cube_target_yaw_angle_errors_range &&
-              pnp_angle.y > -cube_target_yaw_angle_errors_range) {
-            cube_middle_detect_times++;
+          if (cube_middle_detect_times < cube_targeted_detect_flag_times) {
+            if (pnp_angle.y < cube_target_yaw_angle_errors_range &&
+                pnp_angle.y > -cube_target_yaw_angle_errors_range) {
+              cube_middle_detect_times++;
+            } else {
+              RoboSpinCmdUartBuff uart_temp_spin_cmd;
+              uart_temp_spin_cmd.yaw_angle = pnp_angle.y;
+              std::cout << "uart_temp_spin_cmd.yaw_angle" << uart_temp_spin_cmd.yaw_angle << "\n";
+              serial->write((uint8_t *)&uart_temp_spin_cmd, sizeof(uart_temp_spin_cmd));
+            }
           } else {
             RoboSpinCmdUartBuff uart_temp_spin_cmd;
-            uart_temp_spin_cmd.yaw_angle = pnp_angle.y;
-            std::cout << "uart_temp_spin_cmd.yaw_angle" << uart_temp_spin_cmd.yaw_angle << "\n";
-            serial->write((uint8_t *)&uart_temp_spin_cmd, sizeof(uart_temp_spin_cmd));
-          }
-        } else {
-          RoboSpinCmdUartBuff uart_temp_spin_cmd;
-          uart_temp_spin_cmd.yaw_angle = 0.f;
-          for (int i = 0; i < 3; i++) {
-            serial->write((uint8_t *)&uart_temp_spin_cmd, sizeof(uart_temp_spin_cmd));
-            std::cout << "send yaw 0\n";
-            usleep(cube_target_echo_uart_cmd_sleep_time);
-          }
+            uart_temp_spin_cmd.yaw_angle = 0.f;
+            for (int i = 0; i < 3; i++) {
+              serial->write((uint8_t *)&uart_temp_spin_cmd, sizeof(uart_temp_spin_cmd));
+              std::cout << "send yaw 0\n";
+              usleep(cube_target_echo_uart_cmd_sleep_time);
+            }
 
-          cube_middle_detect_times = 0;
-          robo_inf.catch_cube_mode_status.store(CatchMode::go);
+            cube_middle_detect_times = 0;
+            robo_inf.catch_cube_mode_status.store(CatchMode::go);
+          }
+          break;
         }
-        break;
-      }
 
-      case CatchMode::go: {
-        cv::putText(src_img,
-                    "go straight mode",
-                    cv::Point(50, 50),
-                    cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 150, 255), 1);
-        // 通过方框在图像中位置判断
-        if (cude_front_detect_times < cube_targeted_detect_flag_times) {
-          cv::Rect object_rect(object_2d_rect.x + object_2d_rect.width / 2 - 50,
-                               object_2d_rect.y + object_2d_rect.height - 100,
-                               100, 100); // 底部与目标重叠且居中，固定大小的 rect 用以 pnp
-          pnp->solvePnP(object_3d_rect, object_rect, pnp_angle,
-                        pnp_coordinate_mm, pnp_depth);
-          float select_cube_dis = cube_target_distance_offset - pnp_angle.x;
-          if (select_cube_dis < cube_target_distance_errors_range &&
-              select_cube_dis > -cube_target_distance_errors_range) {
-            cude_front_detect_times++;
+        case CatchMode::go: {
+          cv::putText(src_img,
+                      "go straight mode",
+                      cv::Point(50, 50),
+                      cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 150, 255), 1);
+          // 通过方框在图像中位置判断
+          if (cude_front_detect_times < cube_targeted_detect_flag_times) {
+            cv::Rect object_rect(object_2d_rect.x + object_2d_rect.width / 2 - 50,
+                                object_2d_rect.y + object_2d_rect.height - 100,
+                                100, 100); // 底部与目标重叠且居中，固定大小的 rect 用以 pnp
+            pnp->solvePnP(object_3d_rect, object_rect, pnp_angle,
+                          pnp_coordinate_mm, pnp_depth);
+            float select_cube_dis = cube_target_distance_offset - pnp_angle.x;
+            if (select_cube_dis < cube_target_distance_errors_range &&
+                select_cube_dis > -cube_target_distance_errors_range) {
+              cude_front_detect_times++;
+            } else {
+              RoboGoCmdUartBuff uart_temp_go_cmd;
+              uart_temp_go_cmd.distance = select_cube_dis;
+              std::cout << "uart_temp_go_cmd.distance:" << uart_temp_go_cmd.distance << "\n";
+              serial->write((uint8_t *)&uart_temp_go_cmd, sizeof(uart_temp_go_cmd));
+            }
           } else {
             RoboGoCmdUartBuff uart_temp_go_cmd;
-            uart_temp_go_cmd.distance = select_cube_dis;
-            std::cout << "uart_temp_go_cmd.distance:" << uart_temp_go_cmd.distance << "\n";
-            serial->write((uint8_t *)&uart_temp_go_cmd, sizeof(uart_temp_go_cmd));
+            uart_temp_go_cmd.distance = 0.f;
+            for (int i = 0; i < 3; i++) {
+              serial->write((uint8_t *)&uart_temp_go_cmd, sizeof(uart_temp_go_cmd));
+              std::cout << "send stop \n";
+              usleep(cube_target_echo_uart_cmd_sleep_time);
+            }
+
+            robo_inf.catch_cube_mode_status.store(CatchMode::catch_cube);
+            cude_front_detect_times = 0;
           }
-        } else {
-          RoboGoCmdUartBuff uart_temp_go_cmd;
-          uart_temp_go_cmd.distance = 0.f;
-          for (int i = 0; i < 3; i++) {
-            serial->write((uint8_t *)&uart_temp_go_cmd, sizeof(uart_temp_go_cmd));
-            std::cout << "send stop \n";
-            usleep(cube_target_echo_uart_cmd_sleep_time);
+          break;
+        }
+
+        case CatchMode::catch_cube: {
+          cv::putText(src_img,
+                      "catch cube mode",
+                      cv::Point(50, 50),
+                      cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 150, 255), 1);
+          RoboCatchCmdUartBuff uart_temp_catch_cmd;
+          // 发送积木状态
+          //To-do: 取多次识别的结果发送
+          if ((int)res[yolo_res_selected_id].class_id == 0 ||
+              (int)res[yolo_res_selected_id].class_id == 3) {
+            uart_temp_catch_cmd.cube_state = CUBE_UP;
+          } else if ((int)res[yolo_res_selected_id].class_id == 1 ||
+                      (int)res[yolo_res_selected_id].class_id == 4) {
+            uart_temp_catch_cmd.cube_state = CUBE_DOWN;
+          } else if ((int)res[yolo_res_selected_id].class_id == 2 ||
+                      (int)res[yolo_res_selected_id].class_id == 5) {
+            uart_temp_catch_cmd.cube_state = CUBE_STAND;
+          }
+          // 根据方框面积判断积木大小
+          if (object_2d_rect.area() > cube_2_min_area &&
+              object_2d_rect.area() < cube_2_max_area) {
+            uart_temp_catch_cmd.cube_type = CUBE_2;
+          } else if (object_2d_rect.area() > cube_3_min_area &&
+                    object_2d_rect.area() < cube_3_max_area) {
+            uart_temp_catch_cmd.cube_type = CUBE_3;
+          } else if (object_2d_rect.area() > cube_4_min_area &&
+                    object_2d_rect.area() < cube_4_max_area) {
+            uart_temp_catch_cmd.cube_type = CUBE_4;
+          }
+          if (uart_temp_catch_cmd.cube_state == CUBE_STAND) {
+            uart_temp_catch_cmd.cube_type = CUBE_UNCERTAIN;
+          }
+          // 若判断积木大小结果正常则发送
+          if (uart_temp_catch_cmd.cube_type != 0x00) {
+            for (int i = 0; i < 3; i++) {
+              serial->write((uint8_t *)&uart_temp_catch_cmd, sizeof(uart_temp_catch_cmd));
+              std::cout << "catch, rect size:" << object_2d_rect.area() << "rect type:"
+                        << (int)uart_temp_catch_cmd.cube_type << "rect state:" 
+                        << (int)uart_temp_catch_cmd.cube_state << "\n";
+              usleep(cube_target_echo_uart_cmd_sleep_time);
+            }
           }
 
-          robo_inf.catch_cube_mode_status.store(CatchMode::catch_cube);
+          robo_inf.catch_cube_mode_status.store(CatchMode::off);
+          break;
+        }
+
+        case CatchMode::off:
+          cube_middle_detect_times = 0;
           cude_front_detect_times = 0;
-        }
-        break;
+          break;
+
+        default:
+          break;
       }
 
-      case CatchMode::catch_cube: {
-        cv::putText(src_img,
-                    "catch cube mode",
-                    cv::Point(50, 50),
-                    cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 150, 255), 1);
-        RoboCatchCmdUartBuff uart_temp_catch_cmd;
-        // 发送积木状态
-        //To-do: 取多次识别的结果发送
-        if ((int)res[yolo_res_selected_id].class_id == 0 ||
-            (int)res[yolo_res_selected_id].class_id == 3) {
-          uart_temp_catch_cmd.cube_state = CUBE_UP;
-        } else if ((int)res[yolo_res_selected_id].class_id == 1 ||
-                    (int)res[yolo_res_selected_id].class_id == 4) {
-          uart_temp_catch_cmd.cube_state = CUBE_DOWN;
-        } else if ((int)res[yolo_res_selected_id].class_id == 2 ||
-                    (int)res[yolo_res_selected_id].class_id == 5) {
-          uart_temp_catch_cmd.cube_state = CUBE_STAND;
-        }
-        // 根据方框面积判断积木大小
-        if (object_2d_rect.area() > cube_2_min_area &&
-            object_2d_rect.area() < cube_2_max_area) {
-          uart_temp_catch_cmd.cube_type = CUBE_2;
-        } else if (object_2d_rect.area() > cube_3_min_area &&
-                   object_2d_rect.area() < cube_3_max_area) {
-          uart_temp_catch_cmd.cube_type = CUBE_3;
-        } else if (object_2d_rect.area() > cube_4_min_area &&
-                   object_2d_rect.area() < cube_4_max_area) {
-          uart_temp_catch_cmd.cube_type = CUBE_4;
-        }
-        if (uart_temp_catch_cmd.cube_state == CUBE_STAND) {
-          uart_temp_catch_cmd.cube_type = CUBE_UNCERTAIN;
-        }
-        // 若判断积木大小结果正常则发送
-        if (uart_temp_catch_cmd.cube_type != 0x00) {
-          for (int i = 0; i < 3; i++) {
-            serial->write((uint8_t *)&uart_temp_catch_cmd, sizeof(uart_temp_catch_cmd));
-            std::cout << "catch, rect size:" << object_2d_rect.area() << "rect type:"
-                      << (int)uart_temp_catch_cmd.cube_type << "rect state:" 
-                      << (int)uart_temp_catch_cmd.cube_state << "\n";
-            usleep(cube_target_echo_uart_cmd_sleep_time);
-          }
-        }
-
-        robo_inf.catch_cube_mode_status.store(CatchMode::off);
-        break;
-      }
-
-      case CatchMode::off:
-        cube_middle_detect_times = 0;
-        cude_front_detect_times = 0;
-        break;
-
-      default:
-        break;
-      }
-
-#ifndef RELEASE
       cv::rectangle(src_img, object_2d_rect, cv::Scalar(0, 150, 255), 2);
       // 0-blue_yellow, 1-blue_white, 2-blue_blue, 3-red_yellow, 4-red_white,
       // 5-red_red
@@ -223,7 +221,6 @@ void topCameraThread(RoboInf &robo_inf,
                   std::to_string((int)res[yolo_res_selected_id].class_id),
                   cv::Point(object_2d_rect.x, object_2d_rect.y - 1),
                   cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 150, 255), 1);
-#endif
     }
 #ifndef RELEASE
       if (!src_img.empty()) {
@@ -232,10 +229,9 @@ void topCameraThread(RoboInf &robo_inf,
         streamer_ptr->publish("/tpcm",
                               std::string(buff_bgr.begin(), buff_bgr.end()));
       }
+#endif
       cv::resize(src_img, src_img, cv::Size(1800, 900));
       if (!src_img.empty()) cv::imshow("interface", src_img);
-#endif
-      if (cv::waitKey(1) == 'q') break;
     } catch (const std::exception &e) {
       fmt::print("{}\n", e.what());
     }
@@ -269,24 +265,22 @@ void KeyboardThread(std::unique_ptr<CatchKeyboard> &kb, RoboInf &robo_inf) {
   unsigned short code;
   while (true) try {
     if (kb->GetEvent(code)) {
-      switch (code)
-      {
-      case KEY_KP1:
-        robo_inf.catch_cube_mode_status.store(CatchMode::spin);
-        break;
-      case KEY_KP2:
-        robo_inf.catch_cube_mode_status.store(CatchMode::catch_cube);
-        break;
-      case KEY_KP3:
-        robo_inf.catch_cube_mode_status.store(CatchMode::off);
-        break;
-      
-      default:
-        break;
+      switch (code) {
+        case KEY_KP1:
+          robo_inf.catch_cube_mode_status.store(CatchMode::spin);
+          break;
+        case KEY_KP2:
+          robo_inf.catch_cube_mode_status.store(CatchMode::catch_cube);
+          break;
+        case KEY_KP3:
+          robo_inf.catch_cube_mode_status.store(CatchMode::off);
+          break;
+        default:
+          break;
       }
     }
   } catch(const std::exception& e) {
-    std::cerr << e.what() << '\n';
+    fmt::print("{}\n", e.what());
   }
 }
 
